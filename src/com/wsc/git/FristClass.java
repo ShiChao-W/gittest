4ee61b02d6ffcb737ec271cb8d6dff4cbe8168df
shichao-wang <111111>
2019-07-12 16:45:08 +0800

    添加test-git

---
+++ src/com/wsc/git/FristClass.java
@@ -0,0 +1,9 @@
+package com.wsc.git;
+
+public class FristClass {
+    public static void main(String[] args) {
+        System.out.println(hh);
+        System.out.println(hhh);
+        System.out.printf("dfg");
+    }
+}
