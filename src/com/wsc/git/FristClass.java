31126bcbfdf56b749298518f9231eb805c175976
shichao-wang <111111>
2019-07-15 10:15:33 +0800

    修改fristcalss中的内容

--- src/com/wsc/git/FristClass.java
+++ src/com/wsc/git/FristClass.java
@@ -4,11 +4,13 @@ public class FristClass {
     public static void main(String[] args) {
         System.out.println("hh");
         System.out.println("kaldhf");
-        System.out.printf("dfg");
+        System.out.println("dfg");
+        test();
     }
-    public void test(){
+    public static void test() {
         int i=10;
         i +=2;
         System.out.println(i);
     }
 }
+
